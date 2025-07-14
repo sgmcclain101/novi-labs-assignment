eabf33bc7a7ac1981486dab35173b5591a14435f
sgmcclain101 <mcclain.sam@gmail.com>
2025-07-14 10:39:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "nginx" {
-  name                 = "nginx-devops"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = "ngnginx-vpc"
+  name = "nginx-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
@@ -43,35 +34,56 @@ module "security_group" {
   egress_rules        = ["all-all"]
 }
 
-resource "aws_launch_configuration" "nginx_lc" {
-  name_prefix   = "nginx-lc-"
-  image_id      = "ami-0c02fb55956c7d316" # Amazon Linux 2
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecsInstanceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_launch_configuration" "ecs_lc" {
+  name_prefix   = "ecs-lc-"
+  image_id      = "ami-0c02fb55956c7d316" # Amazon Linux 2 ECS-optimized
   instance_type = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
   security_groups = [module.security_group.security_group_id]
   associate_public_ip_address = false
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              docker run -d -p 80:80 nginx
+              echo "ECS_CLUSTER=nginx-cluster" >> /etc/ecs/ecs.config
               EOF
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "nginx_asg" {
-  name_prefix          = "nginx-asg-"
+resource "aws_autoscaling_group" "ecs_asg" {
+  name_prefix          = "ecs-asg-"
   max_size             = 1
   min_size             = 1
   desired_capacity     = 1
   vpc_zone_identifier  = module.vpc.private_subnets
-  launch_configuration = aws_launch_configuration.nginx_lc.name
+  launch_configuration = aws_launch_configuration.ecs_lc.name
 
   tag {
     key                 = "Name"
-    value               = "nginx-instance"
+    value               = "nginx-ecs-instance"
     propagate_at_launch = true
   }
 
@@ -105,17 +117,6 @@ resource "aws_ecs_task_definition" "nginx_task" {
   requires_compatibilities = []
 }
 
-resource "aws_ecs_service" "nginx_service" {
-  name            = "nginx-service"
-  cluster         = aws_ecs_cluster.nginx_cluster.id
-  task_definition = aws_ecs_task_definition.nginx_task.arn
-  launch_type     = "EC2"
-  desired_count   = 1
-  deployment_minimum_healthy_percent = 0
-  deployment_maximum_percent         = 100
-  depends_on = [aws_autoscaling_group.nginx_asg]
-}
-
 resource "aws_lb" "nginx_alb" {
   name               = "nginx-alb"
   internal           = false
@@ -125,10 +126,10 @@ resource "aws_lb" "nginx_alb" {
 }
 
 resource "aws_lb_target_group" "nginx_tg" {
-  name     = "nginx-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  name        = "nginx-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
   target_type = "instance"
 }
 
@@ -141,4 +142,25 @@ resource "aws_lb_listener" "nginx_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.nginx_tg.arn
   }
+}
+
+resource "aws_ecs_service" "nginx_service" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.nginx_cluster.id
+  task_definition = aws_ecs_task_definition.nginx_task.arn
+  launch_type     = "EC2"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.nginx_listener]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.nginx_alb.dns_name
+  description = "DNS name of the Application Load Balancer"
 }
